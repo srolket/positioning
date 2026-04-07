3c64befee1c22e98d2b73008c2cc74cdf3a42fa2
Yury Shevarkov <shevaryury@gmail.com>
2026-04-07 18:51:30 +0500

    final

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const closeBtn = document.getElementById('modalCloseBtn');
 
 function openModal() {
     overlay.style.display = 'flex';
+
+    bar.style.width = '0%';
+    textWhite.style.clipPath = 'inset(0 100% 0 0)';
+
+    animateProgress();
 }
 
 function closeModal() {
@@ -25,3 +30,29 @@ document.addEventListener('keydown', (e) => {
         closeModal();
     }
 });
+
+const bar = document.querySelector('.bar');
+const textWhite = document.querySelector('.text-white');
+
+function animateProgress() {
+    let progress = 0;
+
+    const duration = 3000;
+    const fps = 60;
+    const stepTime = 1000 / fps;
+    const step = 100 / (duration / stepTime);
+
+    const interval = setInterval(() => {
+        progress += step;
+
+        if (progress >= 100) {
+            progress = 100;
+            clearInterval(interval);
+        }
+
+        bar.style.width = progress + '%';
+
+        textWhite.style.clipPath = `inset(0 ${100 - progress}% 0 0)`;
+
+    }, stepTime);
+}
